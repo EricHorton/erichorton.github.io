f4a9a455841b3c06f361ea705d5e35ad88cab53d
EricHorton <ewhorton@ncsu.edu>
2019-10-22 07:57:46 -0400

    Add analytics.

---
+++ js/google-analytics.js
@@ -0,0 +1,13 @@
+/**
+ * Configuration for Google Analytics.
+ *
+ * @module google-analytics
+ */
+
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+gtag('config', 'UA-150644728-1');
